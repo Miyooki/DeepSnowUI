4d66680ab857b858915b79a935770a9c2cc6c5c3
Yuki <35814354+Miyooki@users.noreply.github.com>
2018-03-10 04:09:34 -0500

    asdf

---
+++ app/src/main/java/c/miyooki/snowui/CameraView.java
@@ -0,0 +1,14 @@
+package c.miyooki.snowui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CameraView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_view);
+    }
+}
--- app/src/main/java/c/miyooki/snowui/Login.java
+++ app/src/main/java/c/miyooki/snowui/Login.java
@@ -1,13 +1,27 @@
 package c.miyooki.snowui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        start = findViewById(R.id.button2);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
